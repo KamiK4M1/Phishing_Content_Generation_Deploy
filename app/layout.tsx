58aa192eef344510a0d9bad9138bd0d983d9450c
ชิติพัทธ์ สร้อยสังวาลย์ <116192544+KamiK4M1@users.noreply.github.com>
2025-06-03 21:50:37 +0700

    e

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,19 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Phishing Email Generator',
   description: 'Dev By Kami',
+  icons: {
+    icon: [
+      { url: '/favicon.png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+    ],
+    other: [
+      { rel: 'mask-icon', url: '/safari-pinned-tab.svg', color: '#22c55e' }
+    ]
+  },
 }
 
 export default function RootLayout({
